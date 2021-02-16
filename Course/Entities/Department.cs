3625c626551a98f2044e6c5cf7693fdfd157fb49
Zehtaly <leonardoguths@gmail.com>
2021-02-16 10:06:27 -0300

    Criado projeto Worker do curso de C# do educandoweb.com.br

---
+++ Course/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace Course.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Course/Entities/Worker.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course.Entities
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = departament;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            return BaseSalary + Contracts.Where(x => x.Date.Year == year && x.Date.Month == month).ToList().Sum(x => x.TotalValue());
+        }
+    }
+}
